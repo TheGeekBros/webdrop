203b6b2de7cd6b81aec202351c6afe2c5e4f4867
Umang Galaiya <umang.galaiya@razorpay.com>
2018-10-13 20:21:06 +0530

    Add camera app.

---
+++ camera/main.js
@@ -0,0 +1,78 @@
+const socketConfig = window.config.socket;
+const socket = io(
+  `${socketConfig.procotol}${socketConfig.hostname}:${socketConfig.port}`
+);
+const container = document.querySelector('.container');
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+const screens = {
+  left: document.querySelector('#left'),
+  right: document.querySelector('#right'),
+};
+
+const Events = {
+  CAMERA: 'camera',
+  CAPTURE: 'capture',
+  CAPTURED: 'captured',
+}
+
+const setOrientation = () => {
+  if (window.innerHeight > window.innerWidth) {
+    container.classList.add('portrait');
+  } else {
+    container.classList.add('landscape');
+  }
+};
+
+const setVideo = () => {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        advanced: [{
+          facingMode: "environment"
+        }]
+      }
+    }).then(stream => {
+      screens.left.src = window.URL.createObjectURL(stream);
+      screens.left.play();
+
+      screens.right.src = window.URL.createObjectURL(stream);
+      screens.right.play();
+    });
+  }
+}
+
+const captureHandler = () => {
+  const interval = setInterval(() => {
+    context.drawImage(screens.left, 0, 0, 640, 480);
+
+    const imageData = context.getImageData(0, 0, 640, 480);
+    const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+    if (code) {
+      const {
+        data
+      } = code;
+
+      console.log(data);
+      socket.emit(Events.CAPTURED, {
+        qr: data
+      });
+
+      clearInterval(interval);
+    }
+  }, 100);
+};
+
+const setListeners = () => {
+  socket.on(Events.CAPTURE, captureHandler);
+}
+
+const initApp = () => {
+  socket.emit(Events.CAMERA);
+
+  setOrientation();
+  setVideo();
+  setListeners();
+}
+initApp();
